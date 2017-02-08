97532c54ed79401e18fb8cd423a229e60bb06e19
LarissaMorrell <larissa@larissamorrell.com>
2017-02-07 19:25:37 -0500

    added html pages and tests

--- test/test-server.js
+++ test/test-server.js
@@ -20,3 +20,39 @@ describe('index page', function() {
     });
   });
 });
+
+describe('create account page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/create-account.html')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
+
+describe('user home page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/home.html')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
+
+describe('profile page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/profile.html')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
